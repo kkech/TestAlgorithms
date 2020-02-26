39fd499b13ed70a69be0f6614a12734dc83064c4
Konstantinos <kostas24061992@yahoo.com>
2020-02-26 19:11:52 +0200

    Add JSON

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ from test import Test
 
 test = Test()
 test.connectToDbAndLoadToPandas()
-test.clearData()
-resultList = test.generateTests([[20,2], [30,1], [50, 3]])
+resultList = test.generateTests([[25,5], [25,10], [25, 15], [25,20]],1)
 myfile = open('test.txt', 'w')
 for elem in resultList:
   myfile.write(elem)
--- test.py
+++ test.py
@@ -14,26 +14,42 @@ class Test:
 
   def connectToDbAndLoadToPandas(self):
       print("start")
-      engine = db.create_engine('postgresql://test:test@localhost:5432/test')
+      # engine = db.create_engine('postgresql://test:test@localhost:5432/test')
+      engine = db.create_engine('sqlite:////Users/kechagiaskonstantinos/Downloads/uniJob/testsAlgor/datasets.db')
       connection = engine.connect()
       # metadata = db.MetaData()
       # test = db.Table('testtable', metadata, autoload=True, autoload_with=engine)
       # print(test.columns.keys())
-      self.pdResults = pd.read_sql_table("testtable", con=engine)
+
+      # self.pdResults = pd.read_sql_table("testtable", con=engine)
+
+      self.pdResults = pd.read_sql_table("DATA", con=engine)
+      self.pdResults = self.keepNumericOnly(self.pdResults)
       print(self.pdResults)
 
-  def clearData(self):
-    for col in self.pdResults.columns:
-      print(col)
-    self.pdClearResults = self.pdResults.dropna()
-    print(self.pdClearResults)
+  def keepNumericOnly(self, pdData):
+    # print(pdData.dtypes)
+    pdData = pdData.select_dtypes(include=['number'])
+    # print('++++',pdData)
+    return pdData
 
-  def generateTests(self, testConf):
+  def clearData(self, pdData):
+    for col in pdData.columns:
+      print(col)
+    pdData = pdData.dropna()
+    print(pdData)
+    return pdData
+
+  # Flag = 1 -> Generate one json, Flag = 0 -> Generate multiple json
+  def generateTests(self, testConf, flag):
     # npTestConf = np.array(testConf)
     jsonList = []
     seed(1)
-    for elem in testConf:
-      jsonList.append(self.__makeJson(self.pdClearResults.copy(), elem[0], elem[1]))
+    if flag == 1:
+      jsonList.append(self.__makeJsonProd(self.pdResults.copy(), testConf))
+    else:
+      for elem in testConf:
+        jsonList.append(self.__makeJson(self.pdResults.copy(), elem[0], elem[1]))
     return jsonList
 
   def __runPCA(self, pdData):
@@ -71,6 +87,10 @@ class Test:
       while (len(tempData.columns) != variables):
         dropColNum = randint(0, len(tempData.columns) - 1)
         tempData = tempData.drop(tempData.columns[dropColNum], axis=1)
+      tempData = self.clearData(tempData)
+      if tempData.empty:
+        i = i - 1
+        continue
       columns = "".join(col + "," if ii!=(len(tempData.columns) - 1) else col for ii,col in enumerate(tempData.columns))
       rown, pcaevSorted, pcacSorted = self.__runPCA(tempData)
       pcaevSortedWithoutLn = np.array_repr(pcaevSorted).replace('\n', '')
@@ -81,3 +101,39 @@ class Test:
     jsonString = jsonString + ']}'
     print(jsonString)
     return jsonString
+
+
+  def __makeJsonProd(self, pdData, testConfig):
+    jsonString = '{"result": ['
+
+    t = Template('{ "input": [ { "name": "y", "value": "{{ columns }}" }, { "name": "pathology", "value": "dementia" }, { "name": "dataset", "value": "adni, ppmi, edsd" }, { "name": "filter", "value": "" } ], "output": [ { "name": "n_obs", "value": "{{ rown }}" }, { "name": "eigen_vals", "value": "{{ eigenvalues }}" }, { "name": "eigen_vecs", "value": "{{ eigenvectors }}" } ] }')
+    for elem in testConfig:
+      numOfExp = elem[0]
+      variables = elem[1]
+      for i in range(numOfExp):
+        tempData = pdData.copy()
+        while (len(tempData.columns) != variables):
+          dropColNum = randint(0, len(tempData.columns) - 1)
+          tempData = tempData.drop(tempData.columns[dropColNum], axis=1)
+        tempData = self.clearData(tempData)
+        if tempData.empty:
+          i = i - 1
+          continue
+        columns = "".join(col + "," if ii!=(len(tempData.columns) - 1) else col for ii,col in enumerate(tempData.columns))
+        rown, pcaevSorted, pcacSorted = self.__runPCA(tempData)
+        pcaevSortedWithoutLn = np.array_repr(pcaevSorted).replace('\n', '')
+        pcacSortedWithoutLn = np.array_repr(pcacSorted).replace('\n', '')
+        jsonString = jsonString + t.render(columns=columns, rown=rown, eigenvalues=pcaevSortedWithoutLn, eigenvectors= pcacSortedWithoutLn)
+        jsonString = jsonString + ","
+    jsonString = jsonString[:-1]
+    jsonString = jsonString + ']}'
+    print(jsonString)
+    return jsonString
+# test = Test()
+# test.connectToDbAndLoadToPandas()
+# resultList = test.generateTests([[20,2], [30,1], [50, 3]],1)
+# myfile = open('test.txt', 'w')
+# for elem in resultList:
+#   myfile.write(elem)
+#   myfile.write("\n")
+# myfile.close()
